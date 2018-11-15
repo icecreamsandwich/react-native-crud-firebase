8350c96ee1474e893a559454b6d752d78787d5c2
CrazyRubix <muneebkt@gmail.com>
2018-11-15 22:25:15 +0530

    mods

--- App.js
+++ App.js
@@ -12,16 +12,17 @@ import { View,
   StyleSheet,
   TextInput,
   TouchableHighlight,
+  Alert,
   YellowBox} from 'react-native';
 import { createMaterialTopTabNavigator  } from 'react-navigation';
-// import { db } from './config/db';
-import Firebase from 'firebase';
+//import {Firebase} from 'firebase';
+//import firebase from 'react-native-firebase';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>This is Home! </Text>
+        <Text>This is Home! ok goof nextt!! ok </Text>
       </View>
     );
   }
@@ -30,17 +31,18 @@ class HomeScreen extends React.Component {
 class SettingsScreen extends React.Component {
   constructor(props) {
     super(props);
+	
     this.state = {
-      // firstname: '',
-      // lastname: '',
-      // classn: '',
-      // grade: '',
-      error: false
+		 firstname: '',
+         lastname: '',
+         classn: '',
+         grade: '',
+		 error: false
     }
   }
 
   componentDidMount(){
-    let config = {
+     /*let config = {
       apiKey: "AIzaSyBu7u1NXsPTLkjEV4NGRtxGqLt9BcgiQt0",
       authDomain: "",
       databaseURL: "https://react-native-crud-firebase.firebaseio.com/",
@@ -50,36 +52,34 @@ class SettingsScreen extends React.Component {
     };
 
     let app = Firebase.initializeApp(config);
-    this.db = app.database();
+    this.db = app.database();*/
+	//this.ref = firebase.firestore().collection('items');
   }
  
-  handleChange(e) {
-    const {firstname, lastname, classn, grade} = e.nativeEvent;
-    this.setState({ 
-        firstname: firstname,
-        lastname: lastname,
-        classn: classn,
-        grade: grade,
-    });
-  }
-  handleSubmit() {
-    addItem(this.state.firstname,
-      this.state.lastname,
-      this.state.classn,
-      this.state.grade);
-      AlertIOS.alert(
-     'Item saved successfully'
-    );
-    console.log(this.state.name)
+  handleSubmit = () => {
+     this.addItem(this.state.firstname,
+       this.state.lastname,
+       this.state.classn,
+       this.state.grade);
+       Alert.alert(
+       'Item saved successfully'
+     );
+
   }
 
   addItem =  (firstname,lastname,classn,grade) => {
-    this.db.ref('/items').push({
-        firstname: firstname,
-        lastname: lastname,
-        classn: classn,
-        grade: grade,
-      });
+	/*  this.ref.add({
+      title: 'Added post by random button',
+      likes: Math.floor((Math.random() * 10) + 1),
+      uri: `https://picsum.photos/200/300?image=${Math.floor((Math.random() * 100) + 1)}`,
+    }); */
+	
+     this.db.ref('/items').push({
+         firstname: firstname,
+         lastname: lastname,
+         classn: classn,
+         grade: grade,
+       });
   }
 
   render() {
@@ -89,28 +89,28 @@ class SettingsScreen extends React.Component {
       <TextInput
         name='firstname'
         style={styles.itemInput}
-        onChangeText={firstname => this.setState({firstname})}
+        onChangeText={(text) => this.setState({firstname:text})}
         value={this.state.firstname}
       />
       <Text style={styles.title}>Last Name</Text>
       <TextInput
         name='lastname'
         style={styles.itemInput}
-        onChangeText={lastname => this.setState({lastname})}
+        onChangeText={(text) => this.setState({lastname:text})}
         value={this.state.lastname}
       />
       <Text style={styles.title}>Class</Text>
       <TextInput
         name='classn'
         style={styles.itemInput}
-        onChangeText={classn => this.setState({classn})}
+        onChangeText={(text) => this.setState({classn:text})}
         value={this.state.classn}
       />
       <Text style={styles.title}>Grade</Text>
       <TextInput
         name='grade'
         style={styles.itemInput}
-        onChangeText={grade => this.setState({grade})}
+        onChangeText={(text) => this.setState({grade:text})}
         value={this.state.grade}
       />
       <TouchableHighlight
